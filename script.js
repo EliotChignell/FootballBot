62c5654cfcad8f9608784d3b8d87cbba541a2a25
EliotChignell <chignelleliot@gmail.com>
2023-06-29 11:23:59 +1200

    New website

--- script.js
+++
@@ -1,8 +0,0 @@
-let examples = document.querySelectorAll("#example");
-
-for (var i = 0; i < examples.width; i++) {
-  console.log(`${examples[i].width} ${document.width}`)
-  if (examples[i].width > document.width) {
-    examples[i].width = document.width; 
-  }
-}
